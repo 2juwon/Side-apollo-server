78fffc1348b687a87b26ec2ec63bcc2f3b07061f
devdan <2juwon@gmail.com>
2020-09-08 10:15:54 +0900

    1차 업로드

---
+++ src/database/movies.js
@@ -0,0 +1,24 @@
+const movies = [
+    {
+        id: 1,
+        name: '백두산',
+        rating: 4,
+    },
+    {
+        id: 2,
+        name: '히트맨',
+        rating: 7,
+    },
+    {
+        id: 3,
+        name: '남산의 부장들',
+        rating: 9,
+    },
+    {
+        id: 4,
+        name: '겨울왕국 2',
+        rating: 8,
+    },
+];
+
+export default movies;
